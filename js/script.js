44fa0d68b9e4395fc2f76729e20d476cdd1a4bbf
Arkashu <kopeikaarkashu@gmail.com>
2022-09-18 10:25:15 +0300

    fix this

--- js/script.js
+++ js/script.js
@@ -3,11 +3,11 @@ let ladder = {
     step: 0,
     up: function() {
         this.step++;
-        return ladder;
+        return this;
     },
     down: function() {
         this.step--;
-        return ladder;
+        return this;
     },
     showStep: function() { // показывает текущую ступеньку
         alert( this.step );
